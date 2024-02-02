e3128bca40c4236fe1ff57034854040fba5ad438
Dipsankar <dipsankar2806@gmail.com>
2024-02-02 22:46:18 +0530

    dip

--- app/src/main/java/com/example/wastemanagementtechno/MainActivity.java
+++ app/src/main/java/com/example/wastemanagementtechno/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //this is dummy text ok suvadip is gandu and sameer vai is a big chappri
+        //this is dipsankar branch
     }
 }
